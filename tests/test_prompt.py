54aa805b8adb76d930b861b2999d8caabd03c7a8
mei <mei28aquarius@gmail.com>
2023-05-16 16:17:54 +0900

    chore

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -2,22 +2,6 @@ import unittest
 
 from gptsenpy.Prompt import PromptTemplate
 
-# class TestStringMethods(unittest.TestCase):
-#
-#     def test_upper(self):
-#         self.assertEqual('foo'.upper(), 'FOO')
-#
-#     def test_isupper(self):
-#         self.assertTrue('FOO'.isupper())
-#         self.assertFalse('Foo'.isupper())
-#
-#     def test_split(self):
-#         s = 'hello world'
-#         self.assertEqual(s.split(), ['hello', 'world'])
-#         # check that s.split fails when the separator is not a string
-#         with self.assertRaises(TypeError):
-#             s.split(2)
-
 
 class TestPromptMethods(unittest.TestCase):
     def setUp(self):
